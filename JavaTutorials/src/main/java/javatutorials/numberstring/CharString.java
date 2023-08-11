d7fba9e1e0804a53ea1590dc7eef89750c7c1a57
sunah <1004_sunah@naver.com>
2023-08-12 06:28:13 +0900

    문자열

---
+++ JavaTutorials/src/main/java/javatutorials/numberstring/CharString.java
@@ -0,0 +1,27 @@
+package javatutorials.numberstring;
+
+public class CharString {
+
+    public static void main(String[] args){
+
+        System.out.println("생활코딩");
+
+//        System.out.println('생활코딩');
+
+        System.out.println('생');
+
+        System.out.println("생활코딩" + "입니다");
+
+        System.out.println(1+1);
+
+        System.out.println("1"+"1");
+
+//        System.out.println("egoing said "Welcome programming world"");
+
+        System.out.println("egoing said \"Welcome programming world\"");
+
+        System.out.println();
+
+        System.out.println("HTML\nCSS\nJavaScript\n");
+    }
+}
