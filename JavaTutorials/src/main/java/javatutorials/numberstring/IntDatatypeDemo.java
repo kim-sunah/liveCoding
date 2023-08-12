28496f70900a85fab1862a4eb9380b0acaea9387
sunah <1004_sunah@naver.com>
2023-08-12 22:14:45 +0900

    상수형 데이터타입

---
+++ JavaTutorials/src/main/java/javatutorials/numberstring/IntDatatypeDemo.java
@@ -0,0 +1,10 @@
+package javatutorials.numberstring;
+
+public class IntDatatypeDemo {
+    public static void main(String[] args){
+        byte a;
+//        a = 128;
+        a = -128;
+        System.out.println(a);
+    }
+}
