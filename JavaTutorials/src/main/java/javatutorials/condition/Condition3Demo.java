2a51935e1cac36d8bd458ed75c6af5deb20c502c
sunah <1004_sunah@naver.com>
2023-08-14 03:10:40 +0900

    조건문의 응용

--- JavaTutorials/src/main/java/javatutorials/condition/Condition3Demo.java
+++ JavaTutorials/src/main/java/javatutorials/condition/Condition3Demo.java
@@ -2,6 +2,14 @@ package javatutorials.condition;
 
 public class Condition3Demo {
     public static void main(String[] args) {
+        /**
+         * TODO
+         * InteliJ 에서 Configuration 으로 Argument입력방법
+         *
+         * 1. 구성편집클릭
+         * 2. 프로그램 인수에 값 입력
+         * 3. 저장후 실행
+         */
         if (true) {
             System.out.println(1);
         } else {
