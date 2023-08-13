0dc6490f8cffc6ef73acfd7097771845609daccf
sunah <1004_sunah@naver.com>
2023-08-13 22:44:53 +0900

    연산자

---
+++ JavaTutorials/src/main/java/javatutorials/Operator/Operator.java
@@ -0,0 +1,20 @@
+package javatutorials.Operator;
+
+public class Operator {
+
+    public static void main(String[] args) {
+
+        int result =1+2;
+        System.out.println(result);
+
+        System.out.println(result -1);
+
+        System.out.println(result*2);
+
+        System.out.println(result/2);
+
+        System.out.println((result+8)%7);
+
+
+    }
+}
---
+++ JavaTutorials/src/main/java/javatutorials/Operator/RemainerDemo.java
@@ -0,0 +1,15 @@
+package javatutorials.Operator;
+
+public class RemainerDemo {
+
+    public static void main(String[] args) {
+        int result =3;
+        System.out.println(0 % result);
+        System.out.println(1%result);
+        System.out.println(2%result);
+        System.out.println(3%result);
+        System.out.println(4%result);
+        System.out.println(5%result);
+
+    }
+}
