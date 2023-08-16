6d2e6e1c715f4e91da8df6a9d647af17c1e9878e
sunah <1004_sunah@naver.com>
2023-08-16 23:42:40 +0900

    Method : 출력값1

--- JavaTutorials/src/main/java/javatutorials/method/MethodDemo5.java
+++ JavaTutorials/src/main/java/javatutorials/method/MethodDemo5.java
@@ -1,15 +1,18 @@
 package javatutorials.method;
 
 public class MethodDemo5 {
-    public static void main(String[] args) {
-        numbering(3,5);
+    public static String numbering(int i, int j) {
+        int result = i;
+        String output = "";
+        while (result<j){
+            output += result;
+            result++;
+        }
+        return output;
     }
 
-    private static void numbering(int a, int b) {
-        int i = a;
-        while (i < b) {
-            System.out.println(i);
-            i++;
-        }
+    public static void main(String[] args) {
+        String result = numbering(1,5);
+        System.out.println(result);
     }
 }
