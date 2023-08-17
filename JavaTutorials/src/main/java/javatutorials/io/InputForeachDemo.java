a3accc6759b9121765f2af66f7f5223fdedb27ed
sunah <1004_sunah@naver.com>
2023-08-17 22:33:05 +0900

    앱이 시작할때 데이터를 입력2

---
+++ JavaTutorials/src/main/java/javatutorials/io/InputForeachDemo.java
@@ -0,0 +1,9 @@
+package javatutorials.io;
+
+public class InputForeachDemo{
+    public static void main(String[] args) {
+        for(String e : args){
+            System.out.println(e);
+        }
+    }
+}
