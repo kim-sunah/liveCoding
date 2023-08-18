ca59a4c31ca661c8d04963931260463e5ef7bbc7
sunah <1004_sunah@naver.com>
2023-08-18 23:13:00 +0900

    객채화3

---
+++ JavaTutorials/src/main/java/javatutorials/object/CalculatorDemo3.java
@@ -0,0 +1,32 @@
+package javatutorials.object;
+
+class Calculator {
+    int left, right;
+
+    public void setOprands(int left, int right) {
+        this.left = left;
+        this.right = right;
+    }
+
+    public void avg() {
+        System.out.println((this.left + this.right) / 2);
+    }
+
+    public void sum() {
+        System.out.println(this.left + this.right);
+    }
+}
+
+public class CalculatorDemo3 {
+    public static void main(String[] args) {
+        Calculator c1 = new Calculator();
+        c1.setOprands(10, 20);
+        c1.sum();
+        c1.avg();
+
+        Calculator c2 = new Calculator();
+        c1.setOprands(20, 40);
+        c1.sum();
+        c1.avg();
+    }
+}
