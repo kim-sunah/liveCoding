85cd7f3728e17fa324e45fc4b1f2c2c07a130594
sunah <1004_sunah@naver.com>
2023-08-20 00:17:35 +0900

    클래스 멤버

--- JavaTutorials/src/main/java/javatutorials/classInstance/ClassMemberDemo.java
+++ JavaTutorials/src/main/java/javatutorials/classInstance/ClassMemberDemo.java
@@ -28,14 +28,51 @@ class C1{
 public class ClassMemberDemo {
     public static void main(String[] args) {
         C1 c = new C1();
+        /**
+         * TODO
+         * 인스턴스를 이용해서 정적 메소드에 접근 -> 성공
+         * 인스턴스 메소드가 정적 변수에 접근 -> 성공
+         */
         c.static_static();
-
+        /**
+         * TODO
+         * 인스턴스를  이용해서 정적 메소드에 접근 -> 성공
+         * 정적 메소드가 정적 변수에 접근 -> 실패
+         */
         c.static_instance();
-
+        /**
+         * TODO
+         * 인스턴스를 이용해서 인스턴스 메소드에 접근 -> 성공
+         * 인스턴스 메소드가 클래스 변수에 접근 -> 성공
+         */
         c.instance_static();
+        /**
+         * TODO
+         * 인스턴스를 이용해서 인스턴스 메소드에 접근 -> 성공
+         * 인스턴스 메소드가 인스턴스 변수에 접근 -> 성공
+         */
         c.instance_static();
+        /**
+         * TODO
+         * 클래스를 이용해서 클래스 메소드에 접근 -> 성공
+         * 클래스 메소드가 클래스 변수에 접근 -> 성공
+         */
         C1.static_static();
+        /**
+         * TODO
+         * 인스턴스를 이용해서 인스턴스 메소드에 접근 -> 성공
+         * 인스턴스 메소드가 인스턴스 변수에 접근 -> 실패
+         */
         C1.static_instance();
+        /**
+         * TODO
+         *
+         *  클래스를 이용해서 인스턴스 메소드에 접근 -> 실패
+         *  C1.instance_static();
+         *
+         *  클래스를 이용해서 인스턴스 메소드에 접근 -> 실패
+         *  C1.instatice_instance();
+         */
 
     }
 }
