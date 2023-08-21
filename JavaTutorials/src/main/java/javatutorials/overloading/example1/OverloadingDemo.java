b6db924a94636f4f3b381180358ec7f617c54395
sunah <1004_sunah@naver.com>
2023-08-21 23:12:44 +0900

    overloading 2/2

---
+++ JavaTutorials/src/main/java/javatutorials/overloading/example1/OverloadingDemo.java
@@ -0,0 +1,25 @@
+package javatutorials.overloading.example1;
+
+public class OverloadingDemo {
+
+    void A() {
+        System.out.println("void A()");
+    }
+
+    void A(int arg1) {
+        System.out.println("void A(int arg1)");
+    }
+
+    void A(String arg1) {
+        System.out.println("void A (String arg1)");
+    }
+
+    //        int A(){System.out.println("voidA()");
+    public static void main(String[] args) {
+
+        OverloadingDemo od = new OverloadingDemo();
+        od.A();
+        od.A(1);
+        od.A("coding everybody");
+    }
+}
