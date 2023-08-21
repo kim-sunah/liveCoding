cc1560dec73f3865a951d89ea414eee306f2507e
sunah <1004_sunah@naver.com>
2023-08-21 23:20:37 +0900

    overloading 2/2

---
+++ JavaTutorials/src/main/java/javatutorials/overloading/example1/OverloadingDemo2.java
@@ -0,0 +1,16 @@
+package javatutorials.overloading.example1;
+
+public class OverloadingDemo2 extends OverloadingDemo{
+
+    void A(String arg1, String arg2){System.out.println("sub class : void");}
+
+    void A(){System.out.println("sub class : void A()");}
+
+    public static void main(String[] args) {
+        OverloadingDemo2 od = new OverloadingDemo2();
+        od.A();
+        od.A(1);
+        od.A("coding everybody");
+        od.A("coding everybody", "coding everybody");
+    }
+}
