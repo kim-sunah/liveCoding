d83225e0db1ba426cfab02b294624c813a23c08f
sunah <1004_sunah@naver.com>
2023-08-22 23:52:56 +0900

    로드된 패키지들간의 중복

---
+++ JavaTutorials/src/main/java/javatutorials/packages/example3/D.java
@@ -0,0 +1,10 @@
+package javatutorials.packages.example3;
+
+import javatutorials.packages.example1.*;
+import javatutorials.packages.example2.*;
+
+public class D {
+    public static void main(String[] args) {
+        javatutorials.packages.example1.B b= new javatutorials.packages.example1.B();
+    }
+}
