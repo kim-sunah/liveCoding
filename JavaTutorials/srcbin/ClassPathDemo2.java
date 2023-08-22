824f67fccfaadc54141dbefb0f0840b547e6c2b6
sunah <1004_sunah@naver.com>
2023-08-22 22:40:46 +0900

    컴파일과 클래스 터미널에서 java = 자바파일실행

---
+++ JavaTutorials/srcbin/ClassPathDemo2.java
@@ -0,0 +1,11 @@
+class Item2{
+    public void print(){
+        System.out.println("HelloWorld");
+    }
+}
+class ClasspathDemo2{
+    public static void main(String[] args) {
+        Item2 i1 = new Item2();
+        i1.print();
+    }
+}
