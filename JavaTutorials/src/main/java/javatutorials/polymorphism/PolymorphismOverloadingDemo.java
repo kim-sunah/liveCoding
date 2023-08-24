ff4403971ba80fa4120b11945420fb70ce347d72
sunah <1004_sunah@naver.com>
2023-08-25 03:34:08 +0900

    메소드와 다형성

---
+++ JavaTutorials/src/main/java/javatutorials/polymorphism/PolymorphismOverloadingDemo.java
@@ -0,0 +1,19 @@
+package javatutorials.polymorphism;
+
+class O{
+    public void a(int parm){
+        System.out.println("숫자 출력");
+        System.out.println(parm);
+    }
+    public void a(String param){
+        System.out.println("문자 출력");
+        System.out.println(param);
+    }
+}
+public class PolymorphismOverloadingDemo {
+    public static void main(String[] args) {
+        O o = new O();
+        o.a(1);
+        o.a("one");
+    }
+}
