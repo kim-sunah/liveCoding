9e3a1d2355b4b8f82b1242b449fdd80ece5bc562
sunah <1004_sunah@naver.com>
2023-08-25 03:37:38 +0900

    클래스와 다형성

---
+++ JavaTutorials/src/main/java/javatutorials/polymorphism/PolymorphismOverloadingDemo1.java
@@ -0,0 +1,10 @@
+package javatutorials.polymorphism;
+
+class A{}
+class B extends A{}
+public class PolymorphismOverloadingDemo1 {
+
+    public static void main(String[] args) {
+        A obj = new B();
+    }
+}
