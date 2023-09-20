faff4a40243987a3e3c0eaf719d014acda864f74
sunah <1004_sunah@naver.com>
2023-09-20 15:19:03 +0900

    당근마켓 클론코딩

--- JavaTutorials/src/main/java/javatutorials/classInstance/CalculatorDemo2.java
+++ JavaTutorials/src/main/java/javatutorials/classInstance/CalculatorDemo2.java
@@ -1,5 +1,9 @@
 package javatutorials.classInstance;
 
+
+import java.util.ArrayList;
+import java.util.Collections;
+
 class Calculator2{
     static double PI = 3.14;
     static int base=0;
@@ -15,7 +19,35 @@ class Calculator2{
 
     public void avg(){
         System.out.println((this.left+this.right+base)/2);
+        
+        
     }
+    public int solution(int[] array) {
+        int answer = 0;
+        ArrayList<Integer> intArrayList = new ArrayList<>();
+        
+        return answer;
+    }
+    class Solution {
+    public int solution(int[] array) {
+        int answer = 0;
+        int first = 0;
+        int second = 0;
+        ArrayList<Integer> intArrayList = new ArrayList<>();
+        for(int i : array){
+            intArrayList.add(i);
+        }
+        for(int i : intArrayList){
+            first = Collections.frequency(intArrayList, intArrayList(i));
+        }
+        
+        return answer;
+    }
+
+    private Object intArrayList(int i) {
+        return null;
+    }
+}
 }
 public class CalculatorDemo2 {
 
--- JavaTutorials/src/main/java/javatutorials/test.java
+++ JavaTutorials/src/main/java/javatutorials/test.java
@@ -1,2 +1,28 @@
-package javatutorials;public class test {
+package javatutorials;
+
+import  java.util.*;
+
+public class test {
+    public static void main(String[] args) {
+        int[] array = {6,2,3,6,5,6};
+        solution(array);
+    }
+        public static int solution(int[] array) {
+            int maxCount = 0;
+            int answer = 0;
+            Map<Integer, Integer> map = new HashMap<>();
+            for(int number : array){
+                int count = map.getOrDefault(number, 0) +1;
+                if(count > maxCount){
+                    maxCount = count;
+                    answer = number;
+                }
+                else  if(count == maxCount){
+                    answer = -1;
+                }
+                map.put(number, count);
+            }
+            return answer;
+        }
+
 }
