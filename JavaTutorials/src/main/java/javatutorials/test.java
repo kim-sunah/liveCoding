b10da97004c07d91360dc150e61d3464d8b64cef
sunah <1004_sunah@naver.com>
2023-09-21 00:58:10 +0900

    코딩야학1일차

--- JavaTutorials/src/main/java/javatutorials/test.java
+++ JavaTutorials/src/main/java/javatutorials/test.java
@@ -4,25 +4,11 @@ import  java.util.*;
 
 public class test {
     public static void main(String[] args) {
-        int[] array = {6,2,3,6,5,6};
+        int[] array = {1,2,3,4,5,6};
         solution(array);
     }
-        public static int solution(int[] array) {
-            int maxCount = 0;
-            int answer = 0;
-            Map<Integer, Integer> map = new HashMap<>();
-            for(int number : array){
-                int count = map.getOrDefault(number, 0) +1;
-                if(count > maxCount){
-                    maxCount = count;
-                    answer = number;
-                }
-                else  if(count == maxCount){
-                    answer = -1;
-                }
-                map.put(number, count);
-            }
-            return answer;
-        }
+    public static int[] solution(int[] num_list) {
+        return Arrays.stream(num_list).map(i->i*2).toArray();
+    }
 
 }
